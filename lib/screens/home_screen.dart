1b6ae44e426c76b8e0b34c2e63de7a46cf8cc3c0
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-04-21 01:02:56 +0900

    Home 화면 UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -25,18 +25,15 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              separatorBuilder: (context, index) {
-                return const SizedBox(
-                  width: 10,
-                );
-              },
-              itemBuilder: (context, index) {
-                final webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
             );
           } else {
             return const Center(
@@ -45,6 +42,50 @@ class HomeScreen extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          width: 40,
+        );
+      },
+      itemBuilder: (context, index) {
+        final webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 3,
+                    offset: const Offset(2, 3),
+                    color: Colors.black.withOpacity(0.4),
+                  )
+                ],
+              ),
+              clipBehavior: Clip.hardEdge,
+              width: 180,
+              child: Image.network(
+                webtoon.thumb,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        );
+      },
     );
   }
 }
