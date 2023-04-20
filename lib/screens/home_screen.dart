0a41d84eced2c29b245405332dc97bb5bb2a5c09
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-04-21 00:04:38 +0900

    [INIT] DATA Fetch, Future Type, Named Constructor

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        elevation: 2,
+      ),
+    );
+  }
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  /// Future 타입을 기다리는 async 함수이기 때문에 리턴타입 Future<List<WebtoonModel>>을 명시해준다.
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final webtoonInstance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(webtoonInstance);
+      }
+      return webtoonInstances;
+    } else {
+      throw Error();
+    }
+  }
+}
